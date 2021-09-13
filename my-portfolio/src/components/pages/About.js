4dfeef781e519dc9e46b4cec98205e320b0b37e1
Nickjohn18 <81334326+Nickjohn18@users.noreply.github.com>
2021-09-12 20:51:14 -0400

    initial commit

---
+++ my-portfolio/src/components/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Picture from "./NickJohn.jpg";
+import "./About.css";
+
+const About = () => (
+  <div id="about">
+    <h1>About Me</h1>
+    <div>
+      <img src={Picture} alt="" id="profile-image"></img>
+    </div>
+    <div>
+      <p>
+        My name is Nick John De Luna. I was born in the Philippines and have
+        lived in the United States, Jacksonville, Florida, for most of my life.
+        I've lived in Jacksonville for atleast 8 years and graduated from
+        Sandalwood High School. I first wanted to pursue my talent in
+        Informational Technology but caught interest in programming. Currently
+        attending school to pursue my talent in Computer Science and in the
+        process of acquiring my BS in CS major.
+      </p>
+    </div>
+  </div>
+);
+
+export default About;
---
+++ my-portfolio/src/components/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => (
+  <div>
+    <h1>Contact Page</h1>
+    <section>
+      <div></div>
+    </section>
+  </div>
+);
+
+export default Contact;
